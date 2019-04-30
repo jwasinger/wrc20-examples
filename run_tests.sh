66eea8a85c67ba8104d0716e1c3ccc74187580b9
Jared Wasinger <j-wasinger@hotmail.com>
2019-04-30 05:33:46 +0000

    .

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 cat build/test_list.txt | while read line
 do
-    docker run -v $(pwd)/tests-repo:/tests -t localhost/testeth /build/test/testeth -t GeneralStateTests/stEWASMTests -- --testpath /tests --vm /hera/build/src/libhera.so --singletest $line
+    docker run -v $(pwd)/tests-repo:/tests -t jwasinger/testeth /build/test/testeth -t GeneralStateTests/stEWASMTests -- --testpath /tests --vm /hera/build/src/libhera.so --singletest $line
 done
