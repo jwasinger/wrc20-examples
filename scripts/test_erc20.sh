623529b114b79e4fbd51faab3391b726f0fb55ac
Jared Wasinger <j-wasinger@hotmail.com>
2019-05-20 21:35:47 +0000

    f

--- scripts/test_erc20.sh
+++ scripts/test_erc20.sh
@@ -7,4 +7,4 @@ bash init_geth.sh
 geth_container=$(bash run.sh)
 cd ..
 
-docker run --network host -v $(pwd)/src/C/wrc20.wasm:/build/wrc20.wasm -v $(pwd)/truffle:/project -t jwasinger/truffle sh -c "cd /project && node truffle/deploy-contract.js --wasm src/C/wrc20.wasm && cd truffle && truffle test --network dev"
+docker run --network host -v $(pwd)/src/C/wrc20.wasm:/build/wrc20.wasm -v $(pwd)/truffle:/project -t jwasinger/truffle sh -c "cd /project && node deploy-contract.js --wasm /build/wrc20.wasm && cd truffle && truffle test --network dev"
